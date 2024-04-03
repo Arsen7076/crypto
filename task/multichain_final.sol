5ebeb03f0b8c1441bd8a6d17c68679974833828a
arsen7076 <margaryanarsen83@gmail.com>
2024-04-03 16:00:27 +0400

    add final

--- task/multichain_final.sol
+++ task/multichain_final.sol
@@ -7,19 +7,15 @@ import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.s
 import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/utils/SafeERC20.sol";
 
-/**
- * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
- * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
- * DO NOT USE THIS CODE IN PRODUCTION.
- */
 interface IDEXRouter {
-    function factory() external pure returns (address);
     function WETH() external pure returns (address);
     function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
         external
         payable
         returns (uint[] memory amounts);
 }
+
+
 /// @title - A simple contract for transferring tokens across chains.
 contract TokenTransferor is OwnerIsCreator {
     using SafeERC20 for IERC20;
@@ -47,14 +43,24 @@ contract TokenTransferor is OwnerIsCreator {
     IRouterClient private s_router;  //0xE1053aE1857476f36A3C62580FF9b016E8EE8F6f bnb testnet router
 
     IERC20 private s_linkToken; //0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06
+    address public  ethereumContractAddress; // Ethereum contract address
+    address public stdTokenAddress; // Address of the STD token
 
+    event EthereumContractAddressChanged(address indexed newAddress);
+    event StdTokenAddressChanged(address indexed newAddress);
+    event TokenSwaped(uint indexed  amount);
     /// @notice Constructor initializes the contract with the router address.
-    /// @param _router The address of the router contract.
-    /// @param _link The address of the link contract.
-    constructor(address _router, address _link, address _pancakeSwap) {
-        s_router = IRouterClient(_router);
-        s_linkToken = IERC20(_link);
-        uniswap = IDEXRouter(_pancakeSwap);
+    /// @param _routerChain The address of the router contract.
+    /// @param _uniswapRouter The address of the PancakeSwap router contract.
+    /// @param _ethereumContractAddress The address who will recive tokens.
+    /// @param _stdTokenAddress The address of the PancakeSwap router contract.
+ 
+
+    constructor(address _routerChain, address _uniswapRouter, address _ethereumContractAddress, address _stdTokenAddress) {
+        s_router = IRouterClient(_routerChain);
+        uniswap = IDEXRouter(_uniswapRouter);
+        ethereumContractAddress = _ethereumContractAddress;
+        stdTokenAddress = _stdTokenAddress;
     }
 
     /// @dev Modifier that checks if the chain with the given destinationChainSelector is allowlisted.
@@ -83,72 +89,6 @@ contract TokenTransferor is OwnerIsCreator {
         allowlistedChains[_destinationChainSelector] = allowed;
     }
 
-    /// @notice Transfer tokens to receiver on the destination chain.
-    /// @notice pay in LINK.
-    /// @notice the token must be in the list of supported tokens.
-    /// @notice This function can only be called by the owner.
-    /// @dev Assumes your contract has sufficient LINK tokens to pay for the fees.
-    /// @param _destinationChainSelector The identifier (aka selector) for the destination blockchain.
-    /// @param _receiver The address of the recipient on the destination blockchain.
-    /// @param _token token address.
-    /// @param _amount token amount. 
-    /// @return messageId The ID of the message that was sent.
-    function transferTokensPayLINK(
-        uint64 _destinationChainSelector,  //16015286601757825753 Sepolia 
-        address _receiver,
-        address _token,
-        uint256 _amount
-    )
-        external
-        onlyOwner
-        onlyAllowlistedChain(_destinationChainSelector)
-        validateReceiver(_receiver)
-        returns (bytes32 messageId)
-    {
-        // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
-        //  address(linkToken) means fees are paid in LINK
-        Client.EVM2AnyMessage memory evm2AnyMessage = _buildCCIPMessage(
-            _receiver,
-            _token,
-            _amount,
-            address(s_linkToken)
-        );
-
-        // Get the fee required to send the message
-        uint256 fees = s_router.getFee(
-            _destinationChainSelector,
-            evm2AnyMessage
-        );
-
-        if (fees > s_linkToken.balanceOf(address(this)))
-            revert NotEnoughBalance(s_linkToken.balanceOf(address(this)), fees);
-
-        // approve the Router to transfer LINK tokens on contract's behalf. It will spend the fees in LINK
-        s_linkToken.approve(address(s_router), fees);
-
-        // approve the Router to spend tokens on contract's behalf. It will spend the amount of the given token
-        IERC20(_token).approve(address(s_router), _amount);
-
-        // Send the message through the router and store the returned message ID
-        messageId = s_router.ccipSend(
-            _destinationChainSelector,
-            evm2AnyMessage
-        );
-
-        // Emit an event with message details
-        emit TokensTransferred(
-            messageId,
-            _destinationChainSelector,
-            _receiver,
-            _token,
-            _amount,
-            address(s_linkToken),
-            fees
-        );
-
-        // Return the message ID
-        return messageId;
-    }
 
     /// @notice Transfer tokens to receiver on the destination chain.
     /// @notice Pay in native gas such as ETH on Ethereum or MATIC on Polgon.
@@ -166,10 +106,7 @@ contract TokenTransferor is OwnerIsCreator {
         address _token,
         uint256 _amount
     )
-        external
-        onlyOwner
-        onlyAllowlistedChain(_destinationChainSelector)
-        validateReceiver(_receiver)
+        internal
         returns (bytes32 messageId)
     {
         // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
@@ -290,17 +227,41 @@ contract TokenTransferor is OwnerIsCreator {
         IERC20(_token).safeTransfer(_beneficiary, amount);
     }
 
+    // Allows the contract owner to update the Ethereum contract address
+    function setEthereumContractAddress(address _newAddress) external onlyOwner {
+        require(_newAddress != address(0), "Invalid address");
+        ethereumContractAddress = _newAddress;
+        emit EthereumContractAddressChanged(_newAddress);
+    }
 
-    function swapExactETHforTokens(address tokenOut, uint256 amountIn ,uint256 amountOut, uint256 deadline) external onlyOwner {
+    // Allows the contract owner to update the STD token address
+    function setStdTokenAddress(address _newAddress) external onlyOwner {
+        require(_newAddress != address(0), "Invalid address");
+        stdTokenAddress = _newAddress;
+        emit StdTokenAddressChanged(_newAddress);
+    }
+
+    // Function to swap BNB for STD tokens and then transfer those tokens to the Ethereum contract address
+    function swapAndTransferSTD(uint amountIn, uint amountOutMin, uint deadline, uint64 _destinationChainSelector) external onlyOwner
+        onlyAllowlistedChain(_destinationChainSelector)
+        validateReceiver(ethereumContractAddress) {
+        // Swap BNB for STD tokens
         address[] memory path = new address[](2);
         path[0] = uniswap.WETH();
-        path[1] = tokenOut; //will be STD token
-
-        uniswap.swapExactETHForTokens{value: amountIn}(
-            amountOut,
+        // path[1] = stdTokenAddress; // STD token address
+        path[1] = address(0x337610d27c682E347C9cD60BD4b3b107C9d34dDd);
+        // First swap BNB for STD tokens
+        uint[] memory amounts;
+        amounts = uniswap.swapExactETHForTokens{value: amountIn }(
+            amountOutMin,
             path,
-            msg.sender,
+            address(this), // Tokens need to be received by this contract first
             deadline
         );
+        emit TokenSwaped(amounts[amounts.length - 1]);
+        // Transfer STD tokens to the Ethereum contract address
+        // Assuming the contract already has approval to spend STD tokens on behalf of the owner.
+        transferTokensPayNative(_destinationChainSelector, ethereumContractAddress, stdTokenAddress, amounts[amounts.length - 1]);
     }
+
 }
